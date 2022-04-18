f44221dac67c54053a78267ca408996962503276
AdalbertCosta <contato@adalbertocosta.com>
2022-04-18 16:53:37 -0300

    Finalizando projeto BeautySalon

---
+++ main.js
@@ -0,0 +1,115 @@
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+const links = document.querySelectorAll('nav ul li a')
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* mudar o header da página quando der scroll*/
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    //scroll maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    header.classList.remove('scroll')
+  }
+})
+
+/* Testimonials carrousel - init */
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+
+/* Testimonials carrousel - end */
+
+/* Scroll reveal init (Mostrar elementos quando der scroll na página*/
+ScrollReveal().reveal('.headline')
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  durantion: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .text, #home .image,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials
+    #contact .text, #contact .links,
+    #social .image, #home
+
+`,
+  { interval: 100 }
+)
+/* Scroll reveal init  - end  */
+
+/* Bottom up - init */
+const backToTopButton = document.querySelector('.back-to-top')
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+})
+
+
+/* menu ativo conforme seção visivel na página */
+const sections = document.querySelectorAll('main section[id]')
+function activeMenuAtCurrentSection () {
+
+  const checkpoint = window.pageYOffset + (Window.innerHeight / 8 * 4)
+
+  for( const section of sections ) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if(checkpointStart && checkpointEnd) {
+
+      document.querySelector('nav ul li[herf*=' + sectionId + ']')
+      .classList.add('active')
+
+    } else {
+      document.querySelector('nav ul li[herf*=' + sectionId + ']')
+      .classList.remove('active')
+    }
+  }
+
+}
+
+/* When Scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  activeMenuAtCurrentSection ()
+})
