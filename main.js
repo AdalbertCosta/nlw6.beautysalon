007ea82cb7c27bbc085d0f6cc6b608aeb001c4e7
AdalbertCosta <contato@adalbertocosta.com>
2022-04-18 17:00:30 -0300

    Projeto BeuatySalon - Rocketseat Finalizado

--- main.js
+++ main.js
@@ -1,35 +1,37 @@
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
+
 for (const element of toggle) {
   element.addEventListener('click', function () {
     nav.classList.toggle('show')
   })
 }
 
+/* quando clicar em um item do menu, esconder o menu */
 const links = document.querySelectorAll('nav ul li a')
+
 for (const link of links) {
   link.addEventListener('click', function () {
     nav.classList.remove('show')
   })
 }
 
-/* mudar o header da página quando der scroll*/
-
+/* mudar o header da página quando der scroll */
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
+function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
-    //scroll maior que a altura do header
+    // scroll é maior que a altura do header
     header.classList.add('scroll')
   } else {
     // menor que a altura do header
     header.classList.remove('scroll')
   }
-})
+}
 
-/* Testimonials carrousel - init */
-
+/* Testimonials carousel slider swiper */
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   pagination: {
@@ -45,48 +47,42 @@ const swiper = new Swiper('.swiper-container', {
   }
 })
 
-/* Testimonials carrousel - end */
-
-/* Scroll reveal init (Mostrar elementos quando der scroll na página*/
-ScrollReveal().reveal('.headline')
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
 const scrollReveal = ScrollReveal({
   origin: 'top',
   distance: '30px',
-  durantion: 700,
+  duration: 700,
   reset: true
 })
 
 scrollReveal.reveal(
-  `#home .text, #home .image,
-    #about .image, #about .text,
-    #services header, #services .card,
-    #testimonials header, #testimonials .testimonials
-    #contact .text, #contact .links,
-    #social .image, #home
-
-`,
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
   { interval: 100 }
 )
-/* Scroll reveal init  - end  */
 
-/* Bottom up - init */
+/* Botão voltar para o topo */
 const backToTopButton = document.querySelector('.back-to-top')
-window.addEventListener('scroll', function () {
+
+function backToTop() {
   if (window.scrollY >= 560) {
     backToTopButton.classList.add('show')
   } else {
     backToTopButton.classList.remove('show')
   }
-})
+}
 
-
-/* menu ativo conforme seção visivel na página */
+/* Menu ativo conforme a seção visível na página */
 const sections = document.querySelectorAll('main section[id]')
-function activeMenuAtCurrentSection () {
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
 
-  const checkpoint = window.pageYOffset + (Window.innerHeight / 8 * 4)
-
-  for( const section of sections ) {
+  for (const section of sections) {
     const sectionTop = section.offsetTop
     const sectionHeight = section.offsetHeight
     const sectionId = section.getAttribute('id')
@@ -94,22 +90,21 @@ function activeMenuAtCurrentSection () {
     const checkpointStart = checkpoint >= sectionTop
     const checkpointEnd = checkpoint <= sectionTop + sectionHeight
 
-    if(checkpointStart && checkpointEnd) {
-
-      document.querySelector('nav ul li[herf*=' + sectionId + ']')
-      .classList.add('active')
-
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
     } else {
-      document.querySelector('nav ul li[herf*=' + sectionId + ']')
-      .classList.remove('active')
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
     }
   }
-
 }
 
 /* When Scroll */
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   backToTop()
-  activeMenuAtCurrentSection ()
+  activateMenuAtCurrentSection()
 })
